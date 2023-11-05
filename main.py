cb55ac50dfcc504d0626ce9d7ff42ce1babe1a3b
Yaroslava Mala <ymala@kse.org.ua>
2023-11-05 13:46:53 +0200

    added console logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def encrypt(raw_text, key):
+def encrypt(text, key):
     result = []
 
-    for ch in raw_text:
+    for ch in text:
         if ch.isupper():
             result.append(chr((ord(ch) - 65 + key) % 26 + 65))
         elif ch.islower():
@@ -36,3 +36,25 @@ def readFromFile(path):
 def writeToFile(path, text):
     with open(path, 'w') as file:
         file.write(text)
+
+
+if __name__ == '__main__':
+    while True:
+        command = input("\nEnter 'encrypt' to encrypt a file, 'decrypt' to decrypt a file or 'exit' to quit: ").strip().lower()
+
+        if command == 'exit':
+            print("Exiting the program.")
+            break
+
+        key = int(input("Enter encryption/decryption number-key: "))
+        readFile = input("Enter the name of the input file: ").strip()
+        writeFile = input("Enter the name of the output file: ").strip()
+        text = readFromFile(readFile)
+
+        if command == 'encrypt':
+            result = encrypt(text, key)
+        elif command == 'decrypt':
+            result = decrypt(text, key)
+
+        writeToFile(writeFile, result)
+        print(f"The text has been successfully {command}ed and added in {writeFile}.")
